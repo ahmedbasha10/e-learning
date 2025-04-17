1335649945416bd34b147de328c733fdca75554b
ahmedbasha10 <bashamido6060@gmail.com>
2025-04-17 23:35:57 +0200

    create security configuration.

---
+++ src/main/java/com/logicerror/e_learning/security/config/SecurityConfig.java
@@ -0,0 +1,51 @@
+package com.logicerror.e_learning.security.config;
+
+import com.logicerror.e_learning.security.services.CustomUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    private final CustomUserDetailsService userDetailsService;
+
+    @Autowired
+    public SecurityConfig(CustomUserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        System.out.println("inside security filter chain");
+        http.authenticationProvider(createAuthenticationProvider());
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/login", "/css/**", "/js/**", "/images/**").permitAll();
+            auth.anyRequest().authenticated();
+        });
+
+        http.formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationProvider createAuthenticationProvider() {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return authenticationProvider;
+    }
+
+}
