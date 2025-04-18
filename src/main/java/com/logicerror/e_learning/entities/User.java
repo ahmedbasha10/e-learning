30b2f869a52036234c136d207a66fa1a20441701
ahmedbasha10 <bashamido6060@gmail.com>
2025-04-18 22:30:52 +0200

    update user id column name

--- src/main/java/com/logicerror/e_learning/entities/User.java
+++ src/main/java/com/logicerror/e_learning/entities/User.java
@@ -22,7 +22,7 @@ import java.util.List;
 public class User implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @Column(name = "user_id")
     private Long id;
 
     @Column(name = "first_name", nullable = false)
