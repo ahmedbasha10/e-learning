fa9a550d36d209eeed71eb8d2b9cc15fe3b087e3
ahmedbasha10 <bashamido6060@gmail.com>
2025-04-20 20:11:37 +0200

    add exists methods to user repo

--- src/main/java/com/logicerror/e_learning/repositories/UserRepository.java
+++ src/main/java/com/logicerror/e_learning/repositories/UserRepository.java
@@ -7,4 +7,12 @@ import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+
+    boolean existsByUsername(String username);
+
+    boolean existsByEmailOrUsername(String email, String username);
 }
