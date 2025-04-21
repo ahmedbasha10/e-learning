52ed8af42bc20039f882a2342d0bc2982250785a
ahmedbasha10 <bashamido6060@gmail.com>
2025-04-21 15:52:42 +0200

    remove modelMapper config.

--- src/main/java/com/logicerror/e_learning/config/main/AppConfig.java
+++ src/main/java/com/logicerror/e_learning/config/main/AppConfig.java
@@ -1,14 +1,9 @@
 package com.logicerror.e_learning.config.main;
 
-import org.modelmapper.ModelMapper;
-import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppConfig {
 
-    @Bean
-    public ModelMapper modelMapper() {
-        return new ModelMapper();
-    }
 }
