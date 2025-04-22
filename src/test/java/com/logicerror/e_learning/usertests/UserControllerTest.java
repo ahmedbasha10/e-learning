0467acdb706e15b67210b6dc15a19a4329aa0c36
ahmedbasha10 <bashamido6060@gmail.com>
2025-04-22 22:52:35 +0200

    create user controller test class.

---
+++ src/test/java/com/logicerror/e_learning/usertests/UserControllerTest.java
@@ -0,0 +1,57 @@
+package com.logicerror.e_learning.usertests;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.logicerror.e_learning.dto.RoleDto;
+import com.logicerror.e_learning.requests.CreateUserRequest;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+
+    @Test
+    void registerUser_shouldReturn201AndUserData() throws Exception {
+        long time = System.currentTimeMillis();
+        CreateUserRequest userRequest = new CreateUserRequest(
+                "Ahmed" + time,
+                "ahmed" + time + "@gmail.com",
+                "password123",
+                "Ahmed",
+                "Basha",
+                "Egypt",
+                "Cairo",
+                "Cairo",
+                new RoleDto("role_student")
+        );
+
+        mockMvc.perform(post("/api/v1/users/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(userRequest)))
+                .andExpect(status().isCreated())
+                .andExpect(jsonPath("$.message").value("User registered successfully"))
+                .andExpect(jsonPath("$.data.username").value("Ahmed" + time))
+                .andExpect(jsonPath("$.data.email").value("ahmed" + time + "@gmail.com"))
+                .andExpect(jsonPath("$.data.firstName").value("Ahmed"))
+                .andExpect(jsonPath("$.data.lastName").value("Basha"))
+                .andExpect(jsonPath("$.data.country").value("Egypt"))
+                .andExpect(jsonPath("$.data.city").value("Cairo"))
+                .andExpect(jsonPath("$.data.state").value("Cairo"));
+    }
+}
