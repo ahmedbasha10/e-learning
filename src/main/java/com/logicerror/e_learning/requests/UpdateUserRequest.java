9653b3337782f9e162f79601630e45c82826719e
ahmedbasha10 <bashamido6060@gmail.com>
2025-04-25 23:57:45 +0300

    create update user request

---
+++ src/main/java/com/logicerror/e_learning/requests/UpdateUserRequest.java
@@ -0,0 +1,41 @@
+package com.logicerror.e_learning.requests;
+
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@AllArgsConstructor
+@Data
+public class UpdateUserRequest {
+    @Size(min = 4, max = 20, message = "Username must be between 4 and 20 characters")
+    private String username;
+
+    @Email(message = "Email should be valid")
+    private String email;
+
+    @Size(min = 2, max = 20, message = "First name must be between 2 and 20 characters")
+    private String firstName;
+
+    @Size(min = 2, max = 20, message = "Last name must be between 2 and 20 characters")
+    private String lastName;
+
+    @Pattern(
+            regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&_])[A-Za-z\\d@$!%*?&_]{8,20}$",
+            message = "Password must contain at least one uppercase letter, one lowercase letter, one number, one special character, and be at least 8 characters long and max 20 characters long"
+    )
+    private String password;
+
+    @Override
+    public String toString() {
+        return "UpdateUserRequest{" +
+                "username='" + username + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
