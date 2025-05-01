459af1a82d3e12075bb88f11c87debde40397936
ahmedbasha10 <bashamido6060@gmail.com>
2025-05-01 14:23:46 +0300

    remove unused imports

--- src/main/java/com/logicerror/e_learning/entities/user/User.java
+++ src/main/java/com/logicerror/e_learning/entities/user/User.java
@@ -5,7 +5,6 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
