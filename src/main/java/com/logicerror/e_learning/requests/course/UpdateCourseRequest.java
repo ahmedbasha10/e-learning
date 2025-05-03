83af7430c72f5d9978c3923855b543a503c60e5d
ahmedbasha10 <bashamido6060@gmail.com>
2025-05-03 23:21:05 +0300

    add validation for update course request.

--- src/main/java/com/logicerror/e_learning/requests/course/UpdateCourseRequest.java
+++ src/main/java/com/logicerror/e_learning/requests/course/UpdateCourseRequest.java
@@ -1,13 +1,19 @@
 package com.logicerror.e_learning.requests.course;
 
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @AllArgsConstructor
 @Data
 public class UpdateCourseRequest {
+    @Size(min = 2, max = 100, message = "Title must be between 2 and 100 characters")
     private String title;
+
+    @Size(min = 10, max = 10000, message = "Description must be between 10 and 10000 characters")
     private String description;
+
+
     private String category;
     private String level;
     private Integer duration;
