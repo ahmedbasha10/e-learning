a10a3746ef51e44082d1ed088a3635b1c178338a
ahmedbasha10 <bashamido6060@gmail.com>
2025-05-03 20:02:36 +0300

    add base api path to config.

--- src/main/java/com/logicerror/e_learning/controllers/UserController.java
+++ src/main/java/com/logicerror/e_learning/controllers/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
 
