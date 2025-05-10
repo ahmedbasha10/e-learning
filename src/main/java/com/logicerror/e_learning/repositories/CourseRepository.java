b97b3c9a4bc8f69ea462a9ef13a87bf008b47423
ahmedbasha10 <bashamido6060@gmail.com>
2025-05-10 21:52:00 +0300

    fetch lazy sections.

--- src/main/java/com/logicerror/e_learning/repositories/CourseRepository.java
+++ src/main/java/com/logicerror/e_learning/repositories/CourseRepository.java
@@ -4,6 +4,8 @@ import com.logicerror.e_learning.entities.course.Course;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
@@ -13,6 +15,9 @@ public interface CourseRepository extends JpaRepository<Course, Long> {
     Page<Course> findByCategory(String category, Pageable pageable);
 
     Page<Course> findByLevel(String level, Pageable pageable);
+
+    @Query("SELECT c FROM Course c LEFT JOIN FETCH c.sections WHERE c.id = :courseId")
+    Optional<Course> findByIdWithSections(@Param("courseId") Long courseId);
 
     boolean existsByTitle(String title);
 }
