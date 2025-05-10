1029cc889dc5fd076c8860cd5e358dfdfa1e332f
ahmedbasha10 <bashamido6060@gmail.com>
2025-05-10 21:14:48 +0300

    edit section and video repo

--- src/main/java/com/logicerror/e_learning/repositories/SectionRepository.java
+++ src/main/java/com/logicerror/e_learning/repositories/SectionRepository.java
@@ -1,8 +1,7 @@
 package com.logicerror.e_learning.repositories;
 
 import com.logicerror.e_learning.entities.course.Section;
-import com.logicerror.e_learning.entities.course.SectionKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SectionRepository extends JpaRepository<Section, SectionKey> {
+public interface SectionRepository extends JpaRepository<Section, Long> {
 }
--- src/main/java/com/logicerror/e_learning/repositories/VideoRepository.java
+++ src/main/java/com/logicerror/e_learning/repositories/VideoRepository.java
@@ -1,8 +1,7 @@
 package com.logicerror.e_learning.repositories;
 
 import com.logicerror.e_learning.entities.course.Video;
-import com.logicerror.e_learning.entities.course.VideoKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface VideoRepository extends JpaRepository<Video, VideoKey> {
+public interface VideoRepository extends JpaRepository<Video, Long> {
 }
