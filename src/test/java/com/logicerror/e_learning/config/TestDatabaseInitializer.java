fd971bc030a7fbcf0efa7555b8fcbe24909c8c1c
ahmedbasha10 <bashamido6060@gmail.com>
2025-05-31 17:59:16 +0300

    update test initializer.

--- src/test/java/com/logicerror/e_learning/config/TestDatabaseInitializer.java
+++ src/test/java/com/logicerror/e_learning/config/TestDatabaseInitializer.java
@@ -1,8 +1,13 @@
 package com.logicerror.e_learning.config;
 
+import com.logicerror.e_learning.entities.course.Course;
+import com.logicerror.e_learning.entities.teacher.TeacherCourses;
+import com.logicerror.e_learning.entities.teacher.TeacherCoursesKey;
 import com.logicerror.e_learning.entities.user.Role;
 import com.logicerror.e_learning.entities.user.User;
+import com.logicerror.e_learning.repositories.CourseRepository;
 import com.logicerror.e_learning.repositories.RoleRepository;
+import com.logicerror.e_learning.repositories.TeacherCoursesRepository;
 import com.logicerror.e_learning.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.test.context.TestConfiguration;
@@ -15,7 +20,7 @@ import java.util.List;
 public class TestDatabaseInitializer {
 
     @Bean
-    public CommandLineRunner insertRolesForTests(RoleRepository roleRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CommandLineRunner insertRolesForTests(RoleRepository roleRepository, UserRepository userRepository, CourseRepository courseRepository, TeacherCoursesRepository teacherCoursesRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             List<String> roles = List.of("ROLE_ADMIN", "ROLE_TEACHER", "ROLE_STUDENT");
             for (String roleName : roles) {
@@ -39,6 +44,40 @@ public class TestDatabaseInitializer {
             student.setRole(roleRepository.findByName("ROLE_STUDENT").get());
             User savedUser = userRepository.save(student);
             System.out.println("savedUser = " + savedUser);
+
+            User teacher = new User();
+            teacher.setUsername("teacher");
+            teacher.setPassword(passwordEncoder.encode("password"));
+            teacher.setEmail("teacher@localhost");
+            teacher.setFirstName("teacher");
+            teacher.setLastName("teacher");
+            teacher.setCountry("Country");
+            teacher.setCity("City");
+            teacher.setState("State");
+            teacher.setRole(roleRepository.findByName("ROLE_TEACHER").get());
+            User savedTeacher = userRepository.save(teacher);
+            System.out.println("saved teacher = " + savedUser);
+
+
+            Course course = new Course();
+            course.setTitle("Test Course");
+            course.setDescription("This is a test course for unit testing.");
+            course.setCategory("Test Category");
+            course.setLevel("Beginner");
+            course.setPrice(50);
+            course.setDuration(120);
+            course.setImageUrl("http://example.com/test-course.jpg");
+
+            Course savedCourse = courseRepository.save(course);
+
+            teacherCoursesRepository.save(TeacherCourses.builder()
+                    .course(savedCourse)
+                    .user(savedTeacher)
+                    .id(TeacherCoursesKey.builder()
+                            .courseId(savedCourse.getId())
+                            .userId(savedTeacher.getId())
+                            .build())
+                    .build());
         };
     }
 }
