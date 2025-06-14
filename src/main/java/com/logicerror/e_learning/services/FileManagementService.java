b2727dc7f8a79b03d1e6f831d34a830df7b41967
ahmedbasha10 <bashamido6060@gmail.com>
2025-06-14 17:24:52 +0300

    create file management service.

---
+++ src/main/java/com/logicerror/e_learning/services/FileManagementService.java
@@ -0,0 +1,39 @@
+package com.logicerror.e_learning.services;
+
+import com.logicerror.e_learning.config.StorageProperties;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+
+
+@Service
+public class FileManagementService {
+    private final Path rootLocation;
+
+    public FileManagementService(StorageProperties storageProperties) {
+        this.rootLocation = Path.of(storageProperties.getBasePath());
+    }
+
+    public String uploadFile(InputStream inputStream, String filePath) throws IOException {
+        if (inputStream == null || filePath == null || filePath.isBlank()) {
+            throw new IllegalArgumentException("Input stream and file name must not be null or empty");
+        }
+
+        Path destinationFile = rootLocation.resolve(filePath).normalize().toAbsolutePath();
+
+        if (!destinationFile.startsWith(rootLocation.toAbsolutePath())) {
+            throw new SecurityException("Cannot store file outside current directory.");
+        }
+
+        Files.createDirectories(destinationFile.getParent());
+        Files.copy(inputStream, destinationFile, StandardCopyOption.REPLACE_EXISTING);
+
+        return destinationFile.toString();
+    }
+
+
+}
