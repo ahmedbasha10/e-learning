a273b4114e1293c4e06fb8d9def6bce593760194
ahmedbasha10 <bashamido6060@gmail.com>
2025-06-14 17:24:36 +0300

    create storage properties

---
+++ src/main/java/com/logicerror/e_learning/config/StorageProperties.java
@@ -0,0 +1,18 @@
+package com.logicerror.e_learning.config;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "video-storage")
+@Data
+public class StorageProperties {
+
+    private final String basePath = "storage";
+    private final String videoPath = "videos";
+    private final String imagePath = "images";
+
+    private final long maxFileSize = 524288000; // 500MB
+
+}
