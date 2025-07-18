d2ff59bd1ac06d5c3b63fc9bc77f00497c1b44d0
ahmedbasha10 <bashamido6060@gmail.com>
2025-07-18 19:01:30 +0300

    create FileException

---
+++ src/main/java/com/logicerror/e_learning/exceptions/files/FileException.java
@@ -0,0 +1,7 @@
+package com.logicerror.e_learning.exceptions.files;
+
+public class FileException extends RuntimeException {
+    public FileException(String message) {
+        super(message);
+    }
+}
