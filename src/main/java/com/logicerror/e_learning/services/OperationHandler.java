cc851b81452a536dc5ce3c65a774c27573514a75
ahmedbasha10 <bashamido6060@gmail.com>
2025-07-21 20:33:09 +0300

    setNextHandler void

--- src/main/java/com/logicerror/e_learning/services/OperationHandler.java
+++ src/main/java/com/logicerror/e_learning/services/OperationHandler.java
@@ -2,5 +2,5 @@ package com.logicerror.e_learning.services;
 
 public interface OperationHandler<T> {
     void handle(T courseCreationContext);
-    OperationHandler<T> setNextHandler(OperationHandler<T> nextHandler);
+    void setNextHandler(OperationHandler<T> nextHandler);
 }
