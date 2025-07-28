f5a5b46384142ca715e1e9a17ec67bc862577ab5
ahmedbasha10 <bashamido6060@gmail.com>
2025-07-29 01:30:52 +0300

    add getters and setters.

---
+++ src/main/java/com/logicerror/e_learning/constants/CourseLevel.java
@@ -0,0 +1,22 @@
+package com.logicerror.e_learning.constants;
+
+public enum CourseLevel {
+    BEGINNER("beginner"),
+    INTERMEDIATE("intermediate"),
+    ADVANCED("advanced");
+
+    private final String level;
+
+    CourseLevel(String level) {
+        this.level = level;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    @Override
+    public String toString() {
+        return level;
+    }
+}
--- src/main/java/com/logicerror/e_learning/entities/user/Role.java
+++ src/main/java/com/logicerror/e_learning/entities/user/Role.java
@@ -9,10 +9,6 @@ import lombok.Setter;
 import java.util.Collection;
 
 @Entity
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
 @Table(name = "roles")
 public class Role {
     @Id
@@ -25,4 +21,35 @@ public class Role {
 
     @OneToMany(mappedBy = "role", fetch = FetchType.EAGER)
     private Collection<User> users;
+
+    public Role() {
+    }
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Collection<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(Collection<User> users) {
+        this.users = users;
+    }
 }
