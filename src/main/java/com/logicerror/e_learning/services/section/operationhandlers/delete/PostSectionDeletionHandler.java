3d875473aaf6f734bd20369269a05076bf2c079c
ahmedbasha10 <bashamido6060@gmail.com>
2025-08-10 21:39:00 +0300

    delete section's videos files.

--- src/main/java/com/logicerror/e_learning/services/section/operationhandlers/delete/PostSectionDeletionHandler.java
+++ src/main/java/com/logicerror/e_learning/services/section/operationhandlers/delete/PostSectionDeletionHandler.java
@@ -1,6 +1,8 @@
 package com.logicerror.e_learning.services.section.operationhandlers.delete;
 
+import com.logicerror.e_learning.entities.course.Video;
 import com.logicerror.e_learning.events.SectionRemovedEvent;
+import com.logicerror.e_learning.services.FileManagementService;
 import com.logicerror.e_learning.services.video.CourseProgressDomainService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
@@ -11,12 +13,24 @@ import org.springframework.stereotype.Component;
 public class PostSectionDeletionHandler extends BaseSectionDeletionHandler{
     private final ApplicationEventPublisher eventPublisher;
     private final CourseProgressDomainService courseProgressDomainService;
+    private final FileManagementService fileManagementService;
 
     @Override
     protected void processRequest(SectionDeletionContext context) {
         logger.info("Post section deletion handler started for section: {}", context.getSectionId());
         eventPublisher.publishEvent(new SectionRemovedEvent(this, context.getTargetSection().getCourse().getId()));
         courseProgressDomainService.updateCourseProgressForAllUsers(context.getTargetSection().getCourse().getId());
+        deleteSectionContent(context);
         logger.info("Post section deletion handler completed for section: {}", context.getSectionId());
+    }
+
+    private void deleteSectionContent(SectionDeletionContext context) {
+        // Logic to delete section content goes here
+        logger.info("Deleting content for section: {}", context.getTargetSection().getTitle());
+        // Example: contentRepository.deleteBySectionId(context.getSectionId());
+        for(Video video : context.getTargetSection().getVideos()) {
+            logger.info("Deleting video: {}", video.getTitle());
+            fileManagementService.deleteFile(video.getUrl());
+        }
     }
 }
