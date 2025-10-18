4d3ba90369b2df4feea64bc9d25a97aff2cfe1fa
ahmedbasha10 <bashamido6060@gmail.com>
2025-10-18 20:08:08 +0300

    refactor removing section content logic to use file clean service

--- src/main/java/com/logicerror/e_learning/sections/services/operationhandlers/delete/PostSectionDeletionHandler.java
+++ src/main/java/com/logicerror/e_learning/sections/services/operationhandlers/delete/PostSectionDeletionHandler.java
@@ -1,36 +1,43 @@
 package com.logicerror.e_learning.sections.services.operationhandlers.delete;
 
+import com.logicerror.e_learning.constants.DeletionStatus;
+import com.logicerror.e_learning.entities.course.FileDeletionQueue;
 import com.logicerror.e_learning.entities.course.Video;
 import com.logicerror.e_learning.events.SectionRemovedEvent;
-import com.logicerror.e_learning.services.FileManagementService;
-import com.logicerror.e_learning.services.video.CourseProgressDomainService;
+import com.logicerror.e_learning.repositories.FileDeletionQueueRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Set;
 
 @Component
 @RequiredArgsConstructor
 public class PostSectionDeletionHandler extends BaseSectionDeletionHandler{
     private final ApplicationEventPublisher eventPublisher;
-    private final CourseProgressDomainService courseProgressDomainService;
-    private final FileManagementService fileManagementService;
+    private final FileDeletionQueueRepository fileDeletionQueueRepository;
 
     @Override
     protected void processRequest(SectionDeletionContext context) {
         logger.info("Post section deletion handler started for section: {}", context.getSectionId());
         eventPublisher.publishEvent(new SectionRemovedEvent(this, context.getTargetSection().getCourse().getId()));
-        courseProgressDomainService.updateCourseProgressForAllUsers(context.getTargetSection().getCourse().getId());
         deleteSectionContent(context);
         logger.info("Post section deletion handler completed for section: {}", context.getSectionId());
     }
 
     private void deleteSectionContent(SectionDeletionContext context) {
-        // Logic to delete section content goes here
         logger.info("Deleting content for section: {}", context.getTargetSection().getTitle());
-        // Example: contentRepository.deleteBySectionId(context.getSectionId());
-        for(Video video : context.getTargetSection().getVideos()) {
-            logger.info("Deleting video: {}", video.getTitle());
-            fileManagementService.deleteFile(video.getUrl());
+        Set<Video> videos = context.getTargetSection().getVideos();
+        List<FileDeletionQueue> deletionQueue = videos.stream()
+                .map(video -> new FileDeletionQueue(video.getUrl(), 0, DeletionStatus.PENDING))
+                .toList();
+
+        if (!deletionQueue.isEmpty()) {
+            logger.debug("Adding {} videos to deletion queue for section with ID: {}", deletionQueue.size(), context.getSectionId());
+            fileDeletionQueueRepository.saveAll(deletionQueue);
+        } else {
+            logger.debug("No videos found for section with ID: {}", context.getSectionId());
         }
     }
 }
