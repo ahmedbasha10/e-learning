c2a7d9e42a0c3a4d1932df70903cdddc83b75696
ahmedbasha10 <bashamido6060@gmail.com>
2025-10-18 20:08:33 +0300

    use courseAuthorizationService

--- src/main/java/com/logicerror/e_learning/sections/services/operationhandlers/create/SectionCreationAuthorizationHandler.java
+++ src/main/java/com/logicerror/e_learning/sections/services/operationhandlers/create/SectionCreationAuthorizationHandler.java
@@ -1,8 +1,7 @@
 package com.logicerror.e_learning.sections.services.operationhandlers.create;
 
-import com.logicerror.e_learning.entities.teacher.TeacherCoursesKey;
+import com.logicerror.e_learning.courses.security.CourseAuthorizationService;
 import com.logicerror.e_learning.entities.user.User;
-import com.logicerror.e_learning.repositories.TeacherCoursesRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.access.AccessDeniedException;
@@ -13,30 +12,16 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class SectionCreationAuthorizationHandler extends BaseSectionCreationHandler {
 
-    private final TeacherCoursesRepository teacherCoursesRepository;
+    private final CourseAuthorizationService courseAuthorizationService;
 
     @Override
     protected void processRequest(SectionCreationContext context) {
         User user = context.getUser();
 
-        if (!user.isTeacher()) {
-            log.error("User does not have permission to create a section");
-            throw new AccessDeniedException("User does not have permission to create a section");
+        if(!courseAuthorizationService.hasAccessToModifyCourse(context.getCourseId(), user)) {
+            log.error("User with ID: {} does not have permission to create section in course ID: {}", user.getId(), context.getCourseId());
+            throw new AccessDeniedException("User does not have permission to create section in this course");
         }
-
-
-        boolean isOwner = teacherCoursesRepository.existsById(
-                TeacherCoursesKey.builder()
-                        .courseId(context.getCourseId())
-                        .userId(user.getId())
-                        .build()
-        );
-
-        if (!isOwner) {
-            log.error("User is not the owner of the course");
-            throw new AccessDeniedException("User is not the owner of the course");
-        }
-
 
         log.debug("User has permission to create a section");
     }
