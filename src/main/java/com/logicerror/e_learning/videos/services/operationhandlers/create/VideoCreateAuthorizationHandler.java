7c09f45c00ef7e0de0dfe0d30d1733ca11a38c79
ahmedbasha10 <bashamido6060@gmail.com>
2025-10-24 17:19:49 +0300

    use course authorization service.

--- src/main/java/com/logicerror/e_learning/videos/services/operationhandlers/create/VideoCreateAuthorizationHandler.java
+++ src/main/java/com/logicerror/e_learning/videos/services/operationhandlers/create/VideoCreateAuthorizationHandler.java
@@ -16,21 +16,10 @@ public class VideoCreateAuthorizationHandler extends BaseVideoCreationHandler{
     protected void processRequest(VideoCreationContext context) {
         logger.debug("Processing video creation authorization for user: {}", context.getUser().getUsername());
         User user = context.getUser();
-        if (user == null) {
-            logger.error("User is null, cannot authorize video creation.");
-            throw new IllegalArgumentException("User must be authenticated to create a video.");
-        }
 
-        if (!user.isTeacher()) {
-            logger.error("User {} is not authorized to create a video.", user.getUsername());
-            throw new AccessDeniedException("User is not authorized to create a video.");
-        }
-
-        boolean isOwner = courseAuthorizationService.isCourseOwner(context.getCourseId(), user.getId());
-
-        if (!isOwner) {
-            logger.error("User {} is not the owner of the course with ID {}.", user.getUsername(), context.getCourseId());
-            throw new AccessDeniedException("User is not the owner of the course.");
+        if(!courseAuthorizationService.hasAccessToModifyCourse(context.getCourseId(), user)) {
+            logger.warn("User {} does not have permission to create a video in course with ID {}.", user.getUsername(), context.getCourseId());
+            throw new AccessDeniedException("User does not have permission to create video in this course.");
         }
 
         logger.debug("User {} has permission to create a video in course with ID {}.", user.getUsername(), context.getCourseId());
